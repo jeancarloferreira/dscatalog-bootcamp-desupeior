f4938ac2c33790a4970b844e5dfafee40a10f468
Jean Carlo <jean@codebit.com.br>
2021-05-15 20:14:04 -0300

    CategoryResource

---
+++ backend/src/main/java/com/project1/dscatalog/entities/Category.java
@@ -0,0 +1,51 @@
+package com.project1.dscatalog.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Category implements Serializable {
+
+    private static final long serialVersionUID = -3807035067631374957L;
+
+    private Long id;
+    private String name;
+
+    public Category(){
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+
+        return id != null ? id.equals(category.id) : category.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
