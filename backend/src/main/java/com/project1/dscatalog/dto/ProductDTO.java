f9786d55766b6d2c6aac6d6ff0e9a590495249ba
Jean Carlo <jean@codebit.com.br>
2021-06-08 20:19:55 -0300

    Basic beans validation

--- backend/src/main/java/com/project1/dscatalog/dto/ProductDTO.java
+++ backend/src/main/java/com/project1/dscatalog/dto/ProductDTO.java
@@ -3,6 +3,10 @@ package com.project1.dscatalog.dto;
 import com.project1.dscatalog.entities.Category;
 import com.project1.dscatalog.entities.Product;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.ArrayList;
@@ -14,10 +18,19 @@ public class ProductDTO implements Serializable {
     private static final long serialVersionUID = 4194689090515956787L;
 
     private Long id;
+
+    @Size(min = 5, max = 60, message = "Deve ter entre 5 e 60 caracteres ")
+    @NotBlank(message = "Campo obrigatório")
     private String name;
+
+    @NotBlank(message = "Campo obrigatório")
     private String description;
+
+    @Positive(message = "Preco deve ser um valor positivo")
     private Double price;
     private String imgUrl;
+
+    @PastOrPresent(message = "A data do produto não pode ser futura")
     private Instant date;
 
     private List<CategoryDTO> categories = new ArrayList<>();
--- backend/src/main/java/com/project1/dscatalog/dto/UserDTO.java
+++ backend/src/main/java/com/project1/dscatalog/dto/UserDTO.java
@@ -2,6 +2,9 @@ package com.project1.dscatalog.dto;
 
 import com.project1.dscatalog.entities.User;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
@@ -10,8 +13,12 @@ public class UserDTO implements Serializable {
     private static final long serialVersionUID = 4780360373468230898L;
 
     private Long id;
+
+    @NotBlank(message = "Campo obrigatório")
     private String firstName;
     private String lastName;
+
+    @Email(message = "Favor inserir um e-mail válido")
     private String email;
 
     Set<RoleDTO> roles = new HashSet<>();
