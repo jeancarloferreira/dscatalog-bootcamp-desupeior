10afac4035746feba761f86ba2ed79a9dd2fc32d
Jean Carlo <jean@codebit.com.br>
2021-06-08 21:08:25 -0300

    Custom constraintValidator

--- backend/src/main/java/com/project1/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/project1/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.project1.dscatalog.dto;
 
+import com.project1.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 
     private String password;
